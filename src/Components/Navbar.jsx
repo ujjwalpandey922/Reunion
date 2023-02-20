43a2f371674f5332b704005236546ede2dd04a1e
Ujjwal Pandey <ujjwalpandey922@gmail.com>
2023-02-20 14:05:02 +0530

    Added pictures url

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,13 +1,10 @@
 import "./navbar.css";
-
+import logo from "../assets/logo.png";
 const Navbar = () => {
   return (
     <div className="nav">
       <div className="logo">
-        <img
-          src="https://pixabay.com/get/gb32d869c200cfa3c03bde49ed57d4ce242f0169adc127379828c19a4ffe1de3fee9c186ffd416bc4f342a6edcc36cc02e1908f88255655db4e8809ba1ac8f2fe_640.png"
-          alt="LOGO"
-        />
+        <img src={logo} alt="LOGO" />
       </div>
       <div className="menu">
         <li>Home</li>
